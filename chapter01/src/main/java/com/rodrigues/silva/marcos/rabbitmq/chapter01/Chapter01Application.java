4ed4366d8c41dab5b9d2c47624e4223df37774b7
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-06-14 16:11:18 -0300

    feat(chapter01): simple example

--- chapter01/src/main/java/com/rodrigues/silva/marcos/rabbitmq/chapter01/Chapter01Application.java
+++ chapter01/src/main/java/com/rodrigues/silva/marcos/rabbitmq/chapter01/Chapter01Application.java
@@ -1,10 +1,32 @@
 package com.rodrigues.silva.marcos.rabbitmq.chapter01;
 
+import com.rodrigues.silva.marcos.rabbitmq.chapter01.tut1.RabbitAmqpTutorialsRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Chapter01Application {
+
+	@Profile("usage_message")
+	@Bean
+	public CommandLineRunner usage() {
+		return args -> {
+			System.out.println("This app uses Spring Profiles to control its behavior.");
+			System.out.println("Sample usage: java -jar rabbit-tutorials.jar --spring.profiles.active=hello-world,sender");
+		};
+	}
+
+	@Profile("!usage_message")
+	@Bean
+	public CommandLineRunner tutorial() {
+		return new RabbitAmqpTutorialsRunner();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter01Application.class, args);
